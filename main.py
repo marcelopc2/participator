4c2400140f148bde4727bb5f9f7d53219e69f166
marcelopc2 <marcelopc2@gmail.com>
2024-12-19 23:46:47 -0300

    puff muchas mejoras, agregadas columnas de trabajos entregados

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from decouple import config
 import time
 import io
 from datetime import datetime
+from unidecode import unidecode
 
 # Configuración inicial
-BASE_URL = 'https://canvas.uautonoma.cl/api/v1/'
+BASE_URL = 'https://canvas.uautonoma.cl/api/v1'
 TOKEN = config("TOKEN")
 HEADERS = {"Authorization": f"Bearer {TOKEN}"}
+
 
 def get_students(course_id):
     url = f"{BASE_URL}/courses/{course_id}/enrollments"
@@ -19,7 +21,6 @@ def get_students(course_id):
         response = requests.get(url, headers=HEADERS, params=params)
         if response.status_code == 200:
             students.extend(response.json())
-            # Verifica si hay más páginas
             url = response.links.get('next', {}).get('url')
         else:
             st.error(f"Error {response.status_code}: No se pudo obtener la lista de estudiantes.")
@@ -46,26 +47,55 @@ def get_subaccount_info(sub_account_id):
         st.error(f"Error {response.status_code}: No se pudo obtener la información de la subcuenta.")
         return None
 
+def get_assignments(course_id):
+    assignments = []
+    url = f"{BASE_URL}/courses/{course_id}/assignments"
+    while url:
+        response = requests.get(url, headers=HEADERS)
+        if response.status_code == 200:
+            data = response.json()
+            assignments.extend(data)
+            if 'next' in response.links:
+                url = response.links['next']['url']
+            else:
+                url = None
+        else:
+            st.error(f"Error {response.status_code} al obtener tareas: {response.text}")
+            return []
+    return assignments
+
+def get_submissions(course_id, assignment_id):
+    submissions = []
+    url = f"{BASE_URL}/courses/{course_id}/assignments/{assignment_id}/submissions?per_page=100"
+    while url:
+        response = requests.get(url, headers=HEADERS)
+        if response.status_code == 200:
+            data = response.json()
+            submissions.extend(data)
+            if 'next' in response.links:
+                url = response.links['next']['url']
+            else:
+                url = None
+        else:
+            st.error(f"Error {response.status_code} al obtener entregas: {response.text}")
+            return []
+    return submissions
+
 st.set_page_config(page_title="Participation Checker", page_icon="🚀", layout="wide")
 
 def main():
     st.title("Participación en el curso.")
-    st.write("Con esta app podras encontrar rapidamente que estudiantes participaron y cuales no en un curso de canvas. Recuerda que puedes ordenar la tabla alfabeticamente o por participacion, asi como puedes filtrar solo por usuarios que NO hayan participado")
+    st.write("Con esta app podrás encontrar rápidamente qué estudiantes participaron y cuáles no en un curso de Canvas. Puedes además opcionalmente incluir las columnas para las tareas (excluyendo las autoevaluaciones).")
 
-    # course_id = st.text_input("Ingrese el ID del curso:", "")
-    # ver_participacion = st.button("Ver participación")
-    
     with st.form("my_form"):
         course_id = st.text_input("Ingrese el ID del curso:", "")
+        include_assignments = st.checkbox("Incluir entregas en tareas", value=False)
         ver_participacion = st.form_submit_button("Ver participación")
 
-    # Si el usuario hace clic en "Ver participación"
     if ver_participacion and course_id:
-        start_time = time.time()
+        start_time = time.time()  # Se define start_time aquí
         with st.spinner("Obteniendo información..."):
             students = get_students(course_id)
-            end_time = time.time()
-            tiempo_total = end_time - start_time
 
             if students:
                 data = []
@@ -75,65 +105,108 @@ def main():
                     activity = datetime.strptime(student.get("last_activity_at"), "%Y-%m-%dT%H:%M:%SZ") if student.get("last_activity_at") else None
                     sortable_name_list = student.get('user').get('sortable_name').split(',')
                     rut = student.get('user', {}).get("sis_user_id")
+                    user_id = student.get('user', {}).get('id')
                     data.append({
-                        "Nombres": sortable_name_list[1],
-                        "Apellidos": sortable_name_list[0],
-                        "RUT": f"{rut[:-1]}-{rut[-1]}",
+                        "Nombres": sortable_name_list[1].strip(),
+                        "Apellidos": sortable_name_list[0].strip(),
+                        "RUT": f"{rut[:-1]}-{rut[-1]}" if rut else None,
                         "Correo": student.get('user', {}).get("login_id"),
                         "Matriculado": created.strftime("%d-%m-%Y %H:%M"),
                         "Ultima actividad": activity.strftime("%d-%m-%Y %H:%M") if activity else "Nunca",
                         "Ha participado": participation,
-                })
+                        "user_id": user_id
+                    })
                     
-                # course_info = get_course_info(course_id)
-                # sub_account_info = get_subaccount_info(course_info.get("account_id"))
-
                 df = pd.DataFrame(data)
-                # Guardamos todo en session_state
+
+                # Obtener información del curso y subcuenta
+                course_info = get_course_info(course_id)
+                sub_account_info = get_subaccount_info(course_info.get("account_id"))
+
+                # Si se incluye la opción de buscar tareas y entregas
+                if include_assignments:
+                    assignments = get_assignments(course_id)
+                    filtered_assignments = []
+                    for a in assignments:
+                        normalized_name = unidecode(a['name'].lower())
+                        if 'autoevaluacion' not in normalized_name:
+                            filtered_assignments.append(a)
+
+                    assignment_submissions = {}
+                    for a in filtered_assignments:
+                        submissions = get_submissions(course_id, a['id'])
+                        delivered = set()
+                        for s in submissions:
+                            wfs = s.get('workflow_state')
+                            grd = s.get('grade')
+                            # Consideramos "Entregado" si:
+                            # workflow_state in ['submitted', 'graded'] y grade > 0
+                            if wfs in ['submitted', 'graded']:
+                                if grd is not None:
+                                    try:
+                                        if float(grd) > 0:
+                                            delivered.add(s['user_id'])
+                                    except:
+                                        delivered.add(s['user_id'])
+                                else:
+                                    delivered.add(s['user_id'])
+
+                        assignment_submissions[a['id']] = (a['name'], delivered)
+
+                    for a_id, (a_name, submitted_ids) in assignment_submissions.items():
+                        df[a_name] = df['user_id'].apply(lambda uid: "✔️" if uid in submitted_ids else "❌")
+
+                # Aquí calculamos end_time y tiempo_total inmediatamente después de obtener y procesar todos los datos
+                end_time = time.time()
+                tiempo_total = end_time - start_time
+
+                # Guardamos todo en session_state, incluido el tiempo total
                 st.session_state['df_students'] = df
                 st.session_state['tiempo_total'] = tiempo_total
                 st.session_state['participantes_count'] = df[df["Ha participado"] == "✔️"].shape[0]
                 st.session_state['no_participantes_count'] = df[df["Ha participado"] == "❌"].shape[0]
-                st.session_state['course_info'] = get_course_info(course_id)
-                st.session_state['sub_account_info'] = get_subaccount_info(st.session_state['course_info'].get("account_id"))
+                st.session_state['course_info'] = course_info
+                st.session_state['sub_account_info'] = sub_account_info
+                st.session_state['include_assignments'] = include_assignments
 
-    # Comprobamos si ya tenemos datos en session_state
     if 'df_students' in st.session_state:
         diplomado = f"{st.session_state['sub_account_info'].get('name')} - id: {st.session_state['sub_account_info'].get('id')}"
-        curso = f"{st.session_state['course_info'].get('name')} - id: {course_id}"
-        
+        curso = f"{st.session_state['course_info'].get('name')} - id: {st.session_state['course_info'].get('id')}"
+
         st.markdown(f'<span style="font-size: 28px;">{diplomado}</span>', unsafe_allow_html=True)
         st.markdown(f'<span style="font-size: 22px;">*{curso}*</span>', unsafe_allow_html=True)
         st.markdown(f"**:green[Si participaron:]** {st.session_state['participantes_count']} / **:red[No participaron:]** {st.session_state['no_participantes_count']}")
 
         mostrar_no_participantes = st.checkbox("Mostrar solo no participantes", value=False)
 
+        df = st.session_state['df_students']
         if mostrar_no_participantes:
-            df_to_show = st.session_state['df_students'][st.session_state['df_students']["Ha participado"] == "❌"]
+            df_to_show = df[df["Ha participado"] == "❌"]
         else:
-            df_to_show = st.session_state['df_students']
+            df_to_show = df
+
+        # Ocultamos la columna user_id de la vista
+        if 'user_id' in df_to_show.columns:
+            df_to_show = df_to_show.drop(columns=['user_id'])
 
         st.dataframe(df_to_show, use_container_width=True)
-        st.write(f"**Tiempo de obtención de datos:** {st.session_state['tiempo_total']:.2f} segundos")
-        st.write(f"Cuanto tiempo te ahorraste 😉?")
 
-        # Agregar botón de descarga en Excel
+        # Aquí ya leemos el tiempo desde session_state
+        st.write(f"**Tiempo de obtención de datos:** {st.session_state['tiempo_total']:.2f} segundos")
+        st.write("¿Cuánto tiempo te ahorraste 😉?")
+
+        # Botón de descarga en Excel
         output = io.BytesIO()
         with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-            # Escribir DataFrame en Excel
-            df_to_show.to_excel(writer, index=False, startrow=3, sheet_name='Datos')  # El DataFrame comienza en la fila 4 (startrow=3)
-
-            # Obtener el workbook y worksheet
+            df_to_show.to_excel(writer, index=False, startrow=3, sheet_name='Datos')
             workbook = writer.book
             worksheet = writer.sheets['Datos']
 
-            # Agregar título y subtítulo
             title_format = workbook.add_format({'bold': True, 'font_size': 14})
             subtitle_format = workbook.add_format({'italic': True, 'font_size': 12})
             worksheet.write(0, 0, diplomado, title_format)
             worksheet.write(1, 0, curso, subtitle_format)
 
-            # Definir formatos
             center_format = workbook.add_format({'align': 'center'})
             border_format = workbook.add_format({
                 'border': 1,
@@ -146,16 +219,12 @@ def main():
                 'valign': 'top',
                 'border': 1
             })
-
-            # Formato para ✔️ con borde y color verde
             check_format = workbook.add_format({
                 'border': 1,
                 'align': 'center',
                 'valign': 'vcenter',
                 'font_color': 'green'
             })
-
-            # Formato para ❌ con borde y color rojo
             cross_format = workbook.add_format({
                 'border': 1,
                 'align': 'center',
@@ -163,53 +232,41 @@ def main():
                 'font_color': 'red'
             })
 
-            # Ajustar anchos de columnas con formato centrado
-            worksheet.set_column(0, 0, 30)  # Columna 1
-            worksheet.set_column(1, 1, 30)  # Columna 2
-            worksheet.set_column(2, 2, 15, center_format)  # Columna 3
-            worksheet.set_column(3, 3, 40)  # Columna 4
-            worksheet.set_column(4, len(df_to_show.columns) - 1, 20, center_format)  # Resto de columnas
+            worksheet.set_column(0, 0, 30) 
+            worksheet.set_column(1, 1, 30) 
+            worksheet.set_column(2, 2, 15, center_format)
+            worksheet.set_column(3, 3, 40)
+            worksheet.set_column(4, len(df_to_show.columns) - 1, 20, center_format)
 
-            # Obtener el número de filas y columnas del DataFrame
             max_row, max_col = df_to_show.shape
 
-            # Aplicar formato a los encabezados del DataFrame
             for col_num, value in enumerate(df_to_show.columns.values):
-                worksheet.write(3, col_num, value, header_format)  # Fila de encabezados en startrow=3
+                worksheet.write(3, col_num, value, header_format)
 
-            # Aplicar formato con bordes y colores a cada celda del DataFrame
             for row in range(max_row):
                 for col in range(max_col):
-                    # Obtener el valor de la celda
                     cell_value = df_to_show.iloc[row, col]
-
-                    # Calcular la posición en Excel (fila y columna)
-                    excel_row = row + 4  # startrow=3 + 1 (índice 0)
+                    excel_row = row + 4
                     excel_col = col
-
-                    # Determinar el formato a aplicar
                     if cell_value == '✔️':
                         cell_format = check_format
                     elif cell_value == '❌':
                         cell_format = cross_format
                     else:
                         cell_format = border_format
-
-                    # Escribir la celda con el formato correspondiente
                     worksheet.write(excel_row, excel_col, cell_value, cell_format)
 
-            # Opcional: Ajustar el alto de las filas para mejor visualización
             worksheet.set_default_row(20)
 
-        output.seek(0)      
+        output.seek(0)
         st.download_button(
             label="Descargar un Excel",
             data=output,
-            file_name=f'participacion_curso_id_{course_id}.xlsx',
+            file_name=f'participacion_curso_id_{st.session_state["course_info"].get("id")}.xlsx',
             mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         )
 
-    elif not course_id and ver_participacion:
+    elif ver_participacion and not course_id:
         st.error("Por favor, ingrese un ID de curso válido antes de ver la participación.")
 
 if __name__ == "__main__":
